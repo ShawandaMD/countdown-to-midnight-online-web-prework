b540acc53459fa460f01ab8f978d5878922017f2
Shawanda Davis <shawandadavis93@gmail.com>
2018-11-23 01:48:31 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
-#write your code here
+require "pry"
+require "benchmark"
 
-def countdown
+def countdown(number)
+  
+  while number > 0
+    puts "#{number} SECOND(S)!"
+  number -= 1  
+  end
+ return "HAPPY NEW YEAR!"
+  #binding.pry
 end
+
+countdown(10)
+
+def countdown_with_sleep(int)
+  runtime = Benchmark.measure{ countdown_with_sleep(int) }
+  runtime.real.to_i >= 5
+end 
+
+countdown_with_sleep(3)
+
+
